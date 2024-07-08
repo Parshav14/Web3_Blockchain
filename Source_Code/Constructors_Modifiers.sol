a2f293cd4487fa2d345c5d60a19be213f680bd78
Parshav14 <Parshav769@gmail.com>
2024-07-08 19:24:44 +0530

    Constructors_Modifiers

--- Source_Code/Constructors_Modifiers.sol
+++ Source_Code/Constructors_Modifiers.sol
@@ -2,5 +2,25 @@
 pragma solidity >=0.8.7;
 
 contract blocktrain{
-    
+    address public owner;
+
+    constructor(){
+        owner = msg.sender;
+    }
+
+    uint256 public coins = 0;
+    function add() public {
+        require(coins<=3,"Too many Coins!");
+        coins++;
+    }
+
+    modifier onlyOwner() { // act as filter to any function
+        require(msg.sender == owner,"You are not the owner!");
+        // require()
+        _;
+    }
+
+    function subtract() public onlyOwner{
+        coins--;
+    }
 }
