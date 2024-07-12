2342ea1faf3d348d8571b87365cf6c47e5a5e557
Parshav14 <Parshav769@gmail.com>
2024-07-12 06:52:22 +0530

    Keywords in solidity

---
+++ Source_Code/keyword.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.7;
+
+contract Blocktrain {
+    // tx.gasprice //gas price of trans.
+    // tx.origin // address which initated the trans. at first
+    // msg.value // amount of ether(in wei) sent to smart contract
+    // msg.sender // address which send the trans.
+    // block.timestamp // timestamp of block mined(epochs)
+
+    // ether units
+    // 1 ether = 10^18 wei
+    // 1 wei = 1
+    // 1 szabo = 1e12
+    // 1 finney = 1e15
+
+}
