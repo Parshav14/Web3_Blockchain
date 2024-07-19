b1669839fc54339fb88e97e8f600eb4118b3b417
Parshav14 <Parshav769@gmail.com>
2024-07-19 08:35:49 +0530

    More Variables and Functions

--- Source_Code/Variables_Functions.sol
+++ Source_Code/Variables_Functions.sol
@@ -2,5 +2,30 @@
 pragma solidity ^0.8.7;
 
 contract mychain {
-    
+
+    // Variables Visibility
+
+    int private coins = 0;
+    uint internal iamvar = 0;
+    uint public myvar = 0;
+
+    //  Functions Visibility
+
+    function iAmPublic() public{
+        // do anything
+    }
+
+    function its_Private() private{
+        // do anything
+    }
+
+    function mine() internal{
+        // do anything
+    }
+
+    function random() external{
+        // do anything
+    }
+
+
 }
