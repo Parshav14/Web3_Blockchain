f7658a101b4fc5229691fdd78278343f2269cfbf
Parshav14 <Parshav769@gmail.com>
2024-07-19 20:21:49 +0530

    Errors

--- Source_Code/Error_Handling.sol
+++ Source_Code/Error_Handling.sol
@@ -1,3 +1,20 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.7;
 
+contract checkErrors{
+
+    function hello() public pure{
+        require(5>9, "It's wrong");
+    }
+
+    function parshav() public pure {
+        assert(5==5);
+    }
+
+    function singla() public pure {
+        if(5!=7)
+        {
+            revert();
+        }
+    }
+}
